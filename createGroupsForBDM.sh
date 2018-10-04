c12fbcd0f6d28aaa9d45742e88b77b2122508227
Omar Khan <ok2610@gmail.com>
2018-10-04 16:47:03 +0200

    Create createGroupsForBDM.sh
    This script creates users for Big Data clusters that rely on HDFS.
    The token from settings.conf is deemed necessary only when working with Enterprise GitHub as they don't have raw.githubusercontent... style url.

---
+++ createGroupsForBDM.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+#idea: for x groups/users, fetch pub ssh keys automatically from e.g. a github repo
+# to be used at semester start
+# SETTINGS to be surced from file settings.conf ###########################
+. settings.conf
+### has to contain numberOfGroups, repoURL,  homePrefix, namePrefix
+###########################################################################
+
+echo "#########STARTIN - i ll create $numberOfGroups groups #######################################################"
+
+#!/bin/bash
+if [ -e settings.conf ]
+then
+    echo "found settings"
+else
+    echo "could not find settings"
+    exit 0
+fi
+
+currentGroup=1
+
+while [ "$currentGroup" -le "$numberOfGroups" ]
+
+do
+echo "##################### CREATE GROUP $currentGroup"
+myHomeDir="$homePrefix""$namePrefix""$currentGroup"
+
+############ create user, unless i already have that user
+if [ -d $myHomeDir ]
+then
+    echo "!!! we have a Brian already!"
+else
+    echo "creating group $currentGroup "
+    myCurrentName="$namePrefix""$currentGroup"
+    # add user
+    useradd -m $myCurrentName
+fi
+
+############# get the user's key file from repo
+# fetch the file
+myCurrentFile="$repoURL""$namePrefix""$currentGroup""/authorized_keys""$token"
+ echo "trying to fetch $myCurrentFile "
+###### we probably want a tenny weeny little bit of real error checking here ... like, file exists and such ;)
+wget $myCurrentFile
+mv "./authorized_keys""$token" "./authorized_keys"
+######### checking whether we downloaded a key file
+if [ -e authorized_keys ]
+then
+    echo "ok, found key file and will transfer"
+    myTarget="$homePrefix""$namePrefix""$currentGroup""/.ssh"
+
+        if [ -d $myTarget ]
+        then
+                echo "directory exists, nothing to do"
+                else
+                        echo "creating directory - "
+                        echo $myTarget
+                    mkdir $myTarget
+        fi
+
+    echo "and copying to $myTarget "
+    cp authorized_keys $myTarget
+else
+    echo "could not find key file - ignoring this for now"
+fi
+
+######## Access to Spark and Kafka made easier CLUSTER SPECIFIC
+echo "Updating users .bashrc"
+echo "alias spark-shell='SPARK_MAJOR_VERSION=2 spark-shell'" >> "$homePrefix""$namePrefix""$currentGroup""/.bashrc"
+echo "alias spark-submit='SPARK_MAJOR_VERSION=2 spark-submit'" >> "$homePrefix""$namePrefix""$currentGroup""/.bashrc"
+echo "alias kafka-topics='/usr/hdp/current/kafka-broker/bin/kafka-topics.sh'" >> "$homePrefix""$namePrefix""$currentGroup""/.bashrc"
+
+######## Create group in hdfs
+echo "Switching to hdfs user"
+echo "currentGroup=""$currentGroup" >> settings.conf
+su hdfs << 'EOF'
+. settings.conf
+myCurrentName="$namePrefix""$currentGroup"
+echo "Creating ""$myCurrentName"
+hdfs dfs -mkdir "/user/""$myCurrentName"
+hdfs dfs -chown -R "$myCurrentName"":""$myCurrentName" "/user/""$myCurrentName"
+hdfs dfs -mkdir "/tmp/hadoop-""$myCurrentName"
+hdfs dfs -chmod -R 777 "/tmp/hadoop-""$myCurrentName"
+EOF
+sed -i '$d' settings.conf
+
+echo "chowning the home dir"
+chown -R $myCurrentName:$myCurrentName $myHomeDir
+
+rm authorized_keys
+ let "currentGroup += 1"
+done
+su hdfs << 'EOF'
+hdfs dfsadmin -refreshUserToGroupsMappings
+EOF
+echo
+exit 0
