124218150ea6c89fdc284f8d1773a8809093a97b
Omar Khan <ok2610@gmail.com>
2018-10-09 13:35:12 +0200

    Create createSingleGroupBDM.sh
    Takes group number and github token as input arguments

---
+++ createSingleGroupBDM.sh
@@ -0,0 +1,105 @@
+#!/bin/bash
+#idea: for x groups/users, fetch pub ssh keys automatically from e.g. a github repo
+# to be used at semester start
+# SETTINGS to be surced from file settings.conf ###########################
+. settings.conf
+### has to contain numberOfGroups, repoURL,  homePrefix, namePrefix
+###########################################################################
+
+if [ -e settings.conf ]
+then
+    echo "found settings"
+else
+    echo "could not find settings"
+    exit 0
+fi
+
+if [ -z $1 ]
+then
+        echo "no number supplied"
+        exit 0
+else
+        currentGroup=$1
+        echo "$currentGroup"
+fi
+
+if [ -z $2 ]
+then
+        echo "no token supplied"
+else
+        token=$2
+        echo "$token"
+fi
+
+echo "##################### CREATE GROUP $currentGroup"
+myHomeDir="$homePrefix""$namePrefix""$currentGroup"
+
+############ create user, unless i already have that user
+if [ -d $myHomeDir ]
+then
+    echo "User already exists"
+else
+    echo "creating group $currentGroup "
+    myCurrentName="$namePrefix""$currentGroup"
+    # add user
+    useradd -m $myCurrentName
+    ######## Access to Spark and Kafka easier
+    echo "Updating users .bashrc"
+    echo "alias spark-shell='SPARK_MAJOR_VERSION=2 spark-shell'" >> "$homePrefix""$namePrefix""$currentGroup""/.bashrc"
+    echo "alias spark-submit='SPARK_MAJOR_VERSION=2 spark-submit'" >> "$homePrefix""$namePrefix""$currentGroup""/.bashrc"
+    echo "alias kafka-topics='/usr/hdp/current/kafka-broker/bin/kafka-topics.sh'" >> "$homePrefix""$namePrefix""$currentGroup""/.bashrc"
+
+    ######## Create user in hdfs
+    echo "Switching to hdfs user"
+    echo "currentGroup=""$currentGroup" >> settings.conf
+su hdfs << 'EOF'
+    . settings.conf
+    myCurrentName="$namePrefix""$currentGroup"
+    echo $myCurrentName
+    hdfs dfs -mkdir "/user/""$myCurrentName"
+    hdfs dfs -chown -R "$myCurrentName"":""$myCurrentName" "/user/""$myCurrentName"
+    hdfs dfs -mkdir "/tmp/hadoop-""$myCurrentName"
+    hdfs dfs -chmod -R 777 "/tmp/hadoop-""$myCurrentName"
+EOF
+
+    ######### Data Link
+    ln -s /home/dataextractor/files/data/ "$myHomeDir""/wifi_data"
+
+fi
+
+############# get the user's key file from repo
+# fetch the file
+myCurrentFile="$repoURL""$namePrefix""$currentGroup""/authorized_keys""$token"
+ echo "trying to fetch $myCurrentFile "
+###### we probably want a tenny weeny little bit of real error checking here ... like, file exists and such ;)
+wget $myCurrentFile
+mv "./authorized_keys""$token" "./authorized_keys"
+######### checking whether we downloaded a key file
+if [ -e authorized_keys ]
+then
+    echo "ok, found key file and will transfer"
+    myTarget="$homePrefix""$namePrefix""$currentGroup""/.ssh"
+
+        if [ -d $myTarget ]
+        then
+                echo "directory exists, nothing to do"
+                else
+                        echo "creating directory - "
+                        echo $myTarget
+                    mkdir $myTarget
+        fi
+
+    echo "and copying to $myTarget "
+    cp authorized_keys $myTarget
+else
+    echo "could not find key file - ignoring this for now"
+fi
+
+echo "chowning the home dir"
+chown -R $myCurrentName:$myCurrentName $myHomeDir
+
+rm authorized_keys
+su hdfs << 'EOF'
+hdfs dfsadmin -refreshUserToGroupsMappings
+EOF
+exit 0
